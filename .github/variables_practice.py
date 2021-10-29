2e32e9995e2522392116b13a2aeb548bab1aa637
Kyomari B <90330125+Zonic21342@users.noreply.github.com>
2021-10-29 09:39:45 -0400

    Version 0.2
    Added prints

---
+++ .github/variables_practice.py
@@ -0,0 +1,11 @@
+# Variables Practice, Brunswick Kyomari, 29/10/21 9:38am, version 0.2
+
+# Three Main Types of Variables for this class
+
+health = 420 # INTEGERS, any +/- whole number AND 0
+account_balance = 213.42 # FLOATING POINT, any +/- with decimal.
+user_name = "Thomas Edison" # String, letters/numbers printed on screen
+
+print(health)
+print(account_balance)
+print(user_name)

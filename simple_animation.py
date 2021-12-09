758a768256fe57a32f15d5857eb03b0ac750f511
Kyomari B <90330125+Zonic21342@users.noreply.github.com>
2021-12-09 08:56:09 -0500

    V0.1

--- simple_animation.py
+++ simple_animation.py
@@ -1,1 +1,13 @@
-# Simple Animation with Pygame, Brunswick Kyomari, 12-9-21, 8:45AM v0.0
+# Simple Animation with Pygame, Brunswick Kyomari, 12-9-21, 8:55AM v0.1
++
++import pygame, sys, time
++from pygame.locals import
++
++# Setup Pygame
++pygame.init()
++
++# Setup the Window
++WINDOWWIDTH = 400
++WINDOWHEIGHT = 400
++WindowSurface = Pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
++pygame.display.set_caption('Animation Example')

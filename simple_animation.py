a864dd01076b5aeef19c6009663d3836c799af85
Kyomari B <90330125+Zonic21342@users.noreply.github.com>
2021-12-09 09:01:34 -0500

    V0.2

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with Pygame, Brunswick Kyomari, 12-9-21, 8:55AM v0.1
+# Simple Animation with Pygame, Brunswick Kyomari, 12-9-21, 9:01AM v0.2
 
 import pygame, sys, time
 from pygame.locals import
@@ -11,3 +11,11 @@ WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 WindowSurface = Pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation Example')
+
+# Setup the direction variables.
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4
